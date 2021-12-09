e503be0f0684d83dfdee61616134bc64b4328163
AlexJean <alexjean1031@gmail.com>
2021-12-09 20:21:43 +0800

    be.goodbrain.OnlyHuman empty cocoa project

---
+++ OnlyHuman/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OnlyHuman
+//
+//  Created by Alex on 2021/12/9.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
